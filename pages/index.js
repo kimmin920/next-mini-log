6e9aa0dde2b2b9a99baf502da174b35b5dc7f4ce
minwooKim <vvee1253@gmail.com>
2021-07-08 00:35:54 +0900

    Feat: update index

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,50 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 import Head from 'next/head';
+import Post from '../components/Post';
+import sortByDate from '../utils/sortByDate';
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div>
       <Head>
         <title>minilog</title>
       </Head>
-      <h2>hello</h2>
+      <div className='posts'>
+        {posts.map((post, index) => (
+          <Post
+            post={post}
+            key={index}
+          />
+        ))}
+      </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '');
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8',
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts: posts.sort(sortByDate),
+    },
+  };
 }
